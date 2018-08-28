031e08eae0e19c6c682fe12d55da4c5f1444a16c
Unknown <joehommel1@gmail.com>
2018-08-28 14:40:03 -0600

    some stuff...
    sum stuff

--- Models/Player.cs
+++ Models/Player.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace TheImmortalTrail.Models
 {
     public class Player
     {
-        int Score = 100;
-        string saveKey = "S1";
+        public int Score { get; set; } = 100;
+        public string saveKey { get; set; } = "S1";
 
     }
 }
