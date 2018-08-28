6bee20cc35793008105c779a10875787a124876d
Unknown <joehommel1@gmail.com>
2018-08-28 14:40:03 -0600

    some stuff...
    sum stuff

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,38 +13,115 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Collections.ObjectModel;
+using Windows.UI;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace TheImmortalTrail
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-        }
-        public void NextScene(int score, string saveKey)
-        {
+
+            StartGameLogic();
 
         }
 
-        public void SaveState()
-        {
-
-        }
-
-        public Player LoadState(string loadPath)
+        public async void StartGameLogic()
         {
             Player p = new Player();
+            p.saveKey = "S4";
+            p.Score = 85;
+            //SaveState(p);
+            p = await LoadPlayerSaveFile();
+
+            if (p != null)
+            {
+                Canvasation.Background = new SolidColorBrush(Colors.Green);
+                Button button = new Button();
+                button.Content = $"{p.Score}";
+                Canvasation.Children.Add(button);
+            }
+            else
+            {
+                Canvasation.Background = new SolidColorBrush(Colors.Red);
+            }
+            
+
+        }
+
+        public string NextScene(int score, string saveKey)
+        {
+            string newSaveKey = "";
+
+             
+
+            return newSaveKey;
+        }
+
+        public async void SaveState(Player p)
+        {
+            var savePicker = new FileSavePicker();
+            savePicker.FileTypeChoices.Add("Immortal Trail", new List<String> { ".tit" });
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+
+            if(file != null)
+            {
+                DataContractSerializer ser = new DataContractSerializer(typeof (Player));
+                string contents = "";
+
+                using (var stream = new MemoryStream())
+                {
+                    ser.WriteObject(stream, p);
+                    
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        contents = streamReader.ReadToEnd();
+                    }
+
+                }
+                await FileIO.WriteTextAsync(file, contents);
+            }
+        }
+
+        public async Task<Player> LoadPlayerSaveFile()
+        {
+            Player p = new Player();
+
+            var filePicker = new FileOpenPicker();
+
+            filePicker.FileTypeFilter.Add(".tit");
+
+            var file = await filePicker.PickSingleFileAsync();
+
+            if(file != null)
+            {
+                DataContractSerializer ser = new DataContractSerializer(typeof(Player));
+
+                using (Stream stream = await file.OpenStreamForReadAsync())
+                {
+                    var loadedPlayer = ser.ReadObject(stream) as Player;
+
+                    if (loadedPlayer != null)
+                    {
+                        p = loadedPlayer;
+                    }
+                }
+            }
+
 
             return p;
         }
 
     }
 
-    
+     
 }
